b6e2c7aa7c540a7c7fae260f4f1dd19cf487b0cf
tonihoyer <antonia.hoyer@informatik.hs-fulda.de>
2020-08-09 21:29:43 +0200

    SceneSwitcher

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void HandleInputData(int val){
+        switch (val){
+            case 0:
+                switchScene("dorfScene");
+                break;
+            case 1:
+                switchScene("City");
+                break;
+            case 2:
+                switchScene("kanalisationScene");
+                break;
+            case 3:
+                switchScene("PlayerHome");
+                break;
+            default:
+                switchScene("dorfScene");
+                break;
+        }
+    }
+
+    public void switchScene(string name){
+        SceneManager.LoadScene(name);
+    }
+}
