10061f6b0607323b93f4b5062bc486b4034eeb7d
tonihoyer <antonia.hoyer@informatik.hs-fulda.de>
2020-09-16 10:21:30 +0200

    alle Scenes können in savedData schreiben, noch keine Spiellogik

---
+++ Assets/Scripts/savedData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class savedData : MonoBehaviour
+{
+
+    void createText(){
+        string path = Application.dataPath + "/entscheidungen.txt";
+        if(!File.Exists(path)){
+            File.WriteAllText(path, "Entscheidungen \n\n");
+        }
+
+        string content = "Login date: " + System.DateTime.Now + "\n";
+
+        File.AppendAllText(path, content);
+    }
+
+    void Start(){
+        createText();
+    }
+}
