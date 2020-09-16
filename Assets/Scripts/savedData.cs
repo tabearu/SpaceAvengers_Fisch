38e8502241b3b12d6da17a908c738f6da87a7b56
tonihoyer <antonia.hoyer@informatik.hs-fulda.de>
2020-09-16 11:11:27 +0200

    angefangen flags zu erstellen

--- Assets/Scripts/savedData.cs
+++ Assets/Scripts/savedData.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 using System.IO;
 public class savedData : MonoBehaviour
 {
-
+    //flags, die im Spiel gesetzt werden sollen, wenn etwas Scenen-übergreifend passieren soll
+    //nicht bei reinen Infos
+    bool kampfGewonnen;
+    bool arbeitPuenktlich;
     void createText(){
         string path = Application.dataPath + "/entscheidungen.txt";
         if(!File.Exists(path)){
             File.WriteAllText(path, "Entscheidungen \n\n");
         }
-
+        //Testdaten
         string content = "Login date: " + System.DateTime.Now + "\n";
 
         File.AppendAllText(path, content);
