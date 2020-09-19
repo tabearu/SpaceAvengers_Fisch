2362b7a1c011c9f0b58de24d5bc9aa976e8cc218
tabea <tabea.runzheimer@informatik.hs-fulda.de>
2020-09-19 18:21:30 +0200

    Add Auto-Model, start script for cars in city

---
+++ Assets/Scripts/cars.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cars : MonoBehaviour
+{
+
+    public GameObject auto;
+
+    // Start is called before the first frame update
+    void Start()
+    {           
+        for (int i = 0; i < 10; i++){
+            createCars(Random.Range(0f, 200.0f), Random.Range(0f, 200.0f));
+        }
+    }
+
+    void createCars(float x, float z){
+        var car = Instantiate<GameObject> (auto);
+        car.transform.position = new Vector3(x, 1f, z);
+        car.transform.localScale = new Vector3 (2f, 2f, 3f);
+        //var carRend = gameObject.AddComponent<Renderer> ();
+        //carRend.material.color = Color.red;
+        //car.transform.parent = autos.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
