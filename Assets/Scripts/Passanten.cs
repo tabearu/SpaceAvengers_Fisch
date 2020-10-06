5c7b9fa4b17a749875317a8a5c8516b597abe167
tonihoyer <antonia.hoyer@informatik.hs-fulda.de>
2020-10-06 17:06:36 +0200

    erste animation?

--- Assets/Scripts/Passanten.cs
+++ Assets/Scripts/Passanten.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Passanten : MonoBehaviour
 {
+    GameObject buergersteig;
     // Start is called before the first frame update
     void Start()
     {
-        
+        buergersteig = GameObject.Find("Buergersteige");
+        if(buergersteig != null){
+            Debug.Log("buh");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void OnCollisionEnter(Collision other){
+        Debug.Log(this.name + "OnTriggerEnter mit " + other.gameObject.name);       
     }
 }
