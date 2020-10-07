f112b7dec9bf3c6960eb5d2a8f2f244e69a738ee
tonihoyer <antonia.hoyer@informatik.hs-fulda.de>
2020-10-07 15:28:42 +0200

    Skript zusammengelegt (hinweis blau ist unnötig)

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPC_erkennen : MonoBehaviour
+public class Raycast : MonoBehaviour
 {
+    public Canvas canvas;
     public Camera camera;
     // Start is called before the first frame update
     void Start()
     {
         camera.GetComponent<Camera>();
+        canvas.GetComponent<Canvas>();
     }
 
     // Update is called once per frame
@@ -20,6 +22,14 @@ public class NPC_erkennen : MonoBehaviour
         if(Physics.Raycast(ray, out hit)){
             if(hit.collider.tag == "NPC_interaktiv"){
                Debug.Log("NPC gefunden");
+               if(Input.GetKeyDown(KeyCode.E)){
+                    Debug.Log("angestupst");
+                }
+            }
+            else if(hit.collider.name == "Hinweis_blau"){
+                if(Input.GetKeyDown(KeyCode.E)){
+                    canvas.enabled = true;
+                }
             }
         }
     }
